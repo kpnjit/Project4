14b161853cb847e75917efc8d8d8bd6fade41030
Keerthana <kp79@njit.edu>
2021-08-03 16:29:44 +0530

    Updated files

--- db/init.sql
+++ db/init.sql
@@ -15,3 +15,17 @@ INSERT INTO oscarAgeMale VALUES
 (3,2014,44,'Matthew McConaughey','Dallas Buyers Club'),
 (4,2013,55,'Daniel Day-Lewis','Lincoln'),
 (5,2012,39,'Jean Dujardin','The Artist');
+
+CREATE DATABASE infoDB;
+use infoDB;
+CREATE TABLE IF NOT EXISTS information (
+    `id` int AUTO_INCREMENT,
+    `firstName` VARCHAR(50) CHARACTER SET utf8,
+    `lastName` VARCHAR(50) CHARACTER SET utf8,
+    `address` VARCHAR(150) CHARACTER SET utf8,
+    `email` VARCHAR(50) CHARACTER SET utf8,
+    `password` VARCHAR(50) CHARACTER SET utf8,
+    PRIMARY KEY (`id`)
+);
+INSERT INTO information (firstName, lastName, address, email,password) VALUES
+('test','test','test','test@gmail.com','test')
