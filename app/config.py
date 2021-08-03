e0b299da499a6790afd7f30b99b695131b59de9a
Keerthana <kp79@njit.edu>
2021-08-03 08:31:06 +0530

    Added  files

--- app/config.py
+++ app/config.py
@@ -1,3 +1,10 @@
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))
+
+
 class Config:
     SECRET_KEY = 'AKhsQEIvhPLB678gfKL'
     FLASK_APP = "development"
